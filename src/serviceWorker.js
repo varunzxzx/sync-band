9e0406da506ad894474fad5d959ed62dcd2abf92
varunzxzx <varun995862@gmail.com>
2019-05-09 18:46:05 +0530

    fix ascii character

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -73,9 +73,7 @@ function registerValidSW(swUrl, config) {
                 "New content is available and will be used when all " +
                   "tabs for this page are closed. See https://bit.ly/CRA-PWA."
               );
-              alert(
-                "We've updated the application. Kindly restart the app :smile:"
-              );
+              alert("We've updated the application. Kindly restart the app 😄");
 
               // Execute callback
               if (config && config.onUpdate) {
