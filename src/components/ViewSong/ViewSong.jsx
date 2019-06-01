5992a763d3228a963c045f9759cf68a18f537e35
varunzxzx <varun995862@gmail.com>
2019-06-01 18:48:05 +0530

    inline chords

--- src/components/ViewSong/ViewSong.jsx
+++ src/components/ViewSong/ViewSong.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { Grid, Row, Col } from "rsuite";
 
+import "./ViewSong.css";
+
 function ViewSong(props) {
   const { selectedSong } = props;
   return (
-    <Grid fluid>
+    <Grid className="view-song" fluid>
       <Row>
         <Col md={8} sm={24}>
           <h3>Chords</h3>
           {selectedSong.chords.map(i => (
-            <div key={i}>{i}</div>
+            <div className="chords" key={i}>
+              {i}
+            </div>
           ))}
           {selectedSong.offChords.length !== 0 && (
             <>
               <h3>Off Chords</h3>
               {selectedSong.offChords.map(i => (
-                <div key={i}>{i}</div>
+                <div className="chords" key={i}>
+                  {i}
+                </div>
               ))}
             </>
           )}
           <h3>Transpose</h3>
-          {selectedSong.transpose}
+          <div className="transpose">{selectedSong.transpose}</div>
         </Col>
         <Col md={14} sm={24}>
           <h3>Lyrics</h3>
           <div
+            className="lyrics"
             dangerouslySetInnerHTML={{
               __html: selectedSong.lyrics
             }}
